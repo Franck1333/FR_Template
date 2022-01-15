024317ded49cde316ec09f613a97faf2eab2d87f
Franck Rochat <fun13@live.fr>
2022-01-15 20:05:16 +0100

    Update setup.py
    Ajustements.

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CryptoWatch-Kivy',
-    version="1.13",
+    name='FR_Template',
+    version="1.13.13",
     author='Franck Rochat',
     author_email='rochat.franck@gmail.com',
-    description='This Software allow you to get financials informations about cryptocurrencies by using Python3.',
-    url='https://github.com/Franck1333/CryptoWatch-Kivy',
+    description='The Starter Pack.',
+    url='https://github.com/Franck1333/FR_Template',
     license='lgpl',
     packages=find_packages(),
-    include_package_data=False,
-    install_requires=["cbpro","cmc","pandas","numpy","matplotlib","pydub","cython","kivy"], #Get the Dependencies from Pypi (pip install)
+    include_package_data=True,
+    install_requires=["cbpro","cmc","pandas","numpy","matplotlib","pydub","kivy", "cython"], #Get the Dependencies from Pypi (pip install)
     #dependency_links=[''], #Get the Dependencies via HTTP(s)
 )
